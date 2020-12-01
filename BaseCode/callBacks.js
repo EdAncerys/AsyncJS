172d2a98104c0c66c355a573982eeada24bb9fba
Ed <lookatemail@gmail.com>
2020-12-01 22:21:46 +0000

    Imitating async behaviour in JS

--- BaseCode/callBacks.js
+++ BaseCode/callBacks.js
@@ -1,1 +1,13 @@
-console.log('Hello from callBack');
+'use strict';
+
+console.log('Hello from callBack. CallStack Starts');
+
+const loginUser = (email, password) => {
+  setTimeout(() => {
+    console.log(`User login data: ${email} ${password}`);
+    return { userEmail: email };
+  }, 2000);
+};
+
+loginUser('frodo@email.com', 'password');
+console.log('End of CallStack');
