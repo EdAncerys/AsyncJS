be72d25a8e0ba8578d42480fce1900b72f14c8b1
Ed <lookatemail@gmail.com>
2020-12-01 23:21:18 +0000

    Add asyncAwait example

--- BaseCode/asyncAwait.js
+++ BaseCode/asyncAwait.js
@@ -1,1 +1,35 @@
 'use strict';
+
+const userDataOne = (email) => {
+  setTimeout(() => {
+    console.log('User Data One');
+    return `User Data One email ${email}`;
+    // callBack([`User Data One email ${email}`]);
+  }, 1000);
+};
+
+const userDataTwo = (email) => {
+  setTimeout(() => {
+    console.log('User Data Two');
+    callBack([`User Data Two email ${email}`]);
+  }, 2000);
+};
+
+const userDataThree = (email) => {
+  setTimeout(() => {
+    console.log('User Data Three');
+    callBack([`User Data Three email ${email}`]);
+  }, 3000);
+};
+
+async function asyncCall() {
+  console.log('Making asyncCall');
+
+  const dataOne = await userDataOne('frodo@email.com');
+  const dataTwo = await userDataTwo('ben@email.com');
+  const dataThree = await userDataThree('dover@email.com');
+
+  console.log(userDataOne, userDataTwo, userDataThree);
+}
+
+asyncCall();
